5f69c10f2a6b051f481b55b64cfdc92d0c7a4b91
tjdckscert <tjdcks1051@naver.com>
2024-06-22 16:47:45 +0900

    base path 변경

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/actions_test/'
+  base:'/actions_test/' //저장소 이름으로 맞춰줘야함
 })
